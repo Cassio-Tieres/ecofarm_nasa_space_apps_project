39415d6bd2fcbaa0cb11fe793d2ce7ea36aa40a5
Cássio Tieres Bomfim <tieresbomfim@gmail.com>
2025-10-05 19:11:32 -0300

    Translate comments to english

--- data_process/transform_csv_soil_moisture.py
+++ data_process/transform_csv_soil_moisture.py
@@ -2,22 +2,22 @@ import pandas as pd
 
 df = pd.read_csv('../df/SMAP_L3_SM_P_E_20251003_R19240_001.csv')
 
-# verifica nulos
+# Check for nulls
 print(df.isnull().sum())
 
-# Remove linhas com valores NaNs e gera novo CSV limpo
+# Remove rows with NaN values and generate a new clean CSV
 df = df.dropna()
 
-# Mantém apenas uma amostra (10% da base original) para facilitar a plotagem
+# Keep only a sample (10% of the original dataset) to simplify mean calculation
 df = df.sample(frac=0.1, random_state=1)
 
 df.to_csv('../df/SMAP_L3_SM_P_E_20251003_R19240_001_cleaned.csv', index=False)
 
-# Analisa CSV limpo
+# Analyze cleaned CSV
 df = pd.read_csv('../df/SMAP_L3_SM_P_E_20251003_R19240_001_cleaned.csv')
 print(df.describe())
 
-# retorna dados de Sergipe
+# Return data from Sergipe
 LAT_MIN = -11.5
 LAT_MAX = -9.5
 LON_MIN = -38.5
@@ -33,4 +33,6 @@ sergipe_mask = (
 df_sergipe = df[sergipe_mask]
 
 print(df_sergipe.describe())
+
+# Return the average soil moisture in Sergipe
 print(f"{(df_sergipe['Soil_Moisture'].mean() * 100):.2f}")
